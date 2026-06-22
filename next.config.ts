45a1386aaec0b0658d47479b96f99b9c9c3672e5
KANISHK BHANDARI <kunnubhandari4@gmail.com>
2026-06-23 00:43:01 +0530

    Configure frontend to forward wp requests

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['whatsapp-web.js', 'puppeteer'],
+  /* config options here */
 };
 
 export default nextConfig;
